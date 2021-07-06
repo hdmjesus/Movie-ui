98111b80d673164ab0c63b01f0fa0b1120fe8a7c
Jesus Hernandez <hdmjesus@gmail.com>
2021-07-06 18:29:11 -0400

    fix rutas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MovieUI } from './MovieUi';
 
+
 import '@styles/sass/styles.scss';
 
 ReactDOM.render(<MovieUI />, document.getElementById('root'));
